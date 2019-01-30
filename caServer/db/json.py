925b1114f1086720ca82f16cbafec3cd1a141a23
Artem Sviridov <tim@tech-central.org>
2019-01-30 04:03:53 -0500

    json

--- caServer/db/json.py
+++ caServer/db/json.py
@@ -3,21 +3,19 @@ import json
 
 class JSONDatabase():
 
-    def __init__(self, path, name):
+    def __init__(self, path):
         fl = open(path)
         self.path = path
         self.data = json.load(fl)
-        self.name = name
         fl.close()
-        self.data = self.date[name]
 
     def __getattr__(self, name):
         return self.data[name]
 
     def save(self):
         fl = open(self.path, 'wr')
-        jdata = json.load(fl)
-        jdata[self.name] = self.data
+        #jdata = json.load(fl)
+        jdata = self.data
         jdata.seek(0)
         json.dump(jdata, fl)
         jdata.truncate()
