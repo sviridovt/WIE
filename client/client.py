c49beecb0a4c70e355e520849c57c54db58b9833
istvan <istvan.vonfedak@miami.edu>
2019-02-13 23:33:16 -0500

    Added simple certificate functionality

--- client/client.py
+++ client/client.py
@@ -1,10 +1,39 @@
 import socket
+import json
 
+certFile = 'certificates.json'
 HOST = '127.0.0.1'
 PORT = 4444
+
+# read database of certificates
+with open(certFile, 'r') as fin:
+	certificates = json.load(fin)
+# make sure that file exists
+if certificates is None :
+	raise ValueError()
+
+# init  socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to host on a given port
 s.connect((HOST, PORT))
-s.send(str.encode('Hello, world'))
-data = s.recv(1024)
+
+# send ping request
+s.send(str.encode('ping'))
+
+# read recieved data
+certificate = s.recv(1024)
+
+# try to find certificate in certificates
+try:
+	value = certificates[certificate.decode('utf-8')]
+	print(certificate.decode('utf-8'), value)
+	s.send(str.encode('Acknowledged'))
+# if value not found notify user
+except KeyError:
+	print('certificate not found')
+	s.send(str.encode('Go awway!'))
+
+# close socket
 s.close()
-print ('Received', data.decode())
+
