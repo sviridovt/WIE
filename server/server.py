68a6e82179a26625fe8521a2d489095e2187531f
Istvan <istvan.vonfedak@miami.edu>
2019-02-13 23:51:01 -0500

    Added certificates to server.py

--- server/server.py
+++ server/server.py
@@ -2,13 +2,47 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 4444
+certificate = 'starbucks '
+
+# init socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind to the host and the port
 s.bind((HOST, PORT))
-s.listen(1)
+
+# setting a back long for data size
+s.listen()
+
+# accepting incomming connection
 conn, addr = s.accept()
+
+# printing connected address
 print('Connected by', addr)
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data)
+
+# reading all of the data from the socket
+def readData(conn):
+        data = conn.recv(1024).decode('utf-8')
+        if not data:
+                conn.close()
+                raise ValueError()
+        return data
+
+# if data is read, print
+print('Recieved', readData(conn))
+
+# send certificate
+conn.send(certificate.encode('utf-8'))
+
+# read if acknowledged
+print('Recieved', readData(conn))
+
+while True:
+        data = conn.recv(1024).decode('utf-8')
+        if not data: break
+        print('Recieved', data)
+        conn.send(data)
+
+
+
+
 conn.close()
