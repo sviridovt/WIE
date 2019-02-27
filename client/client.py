0d4a5d06866f92b27a9fa076b22dedfbb91c759a
Istvan von Fedak <istvan.vonfedak@gmail.com>
2019-02-27 11:19:59 -0500

    Merged master with istvan

--- client/client.py
+++ client/client.py
@@ -1,9 +1,14 @@
 import socket
 import json
+from RSAKeys import genKeyPair
+from RSAKeys import encrypt
 
 certFile = 'certificates.json'
 HOST = '127.0.0.1'
 PORT = 4444
+
+# generate RSA key pair
+genKeyPair()
 
 # read database of certificates
 with open(certFile, 'r') as fin:
@@ -21,12 +26,13 @@ s.connect((HOST, PORT))
 # send ping request
 s.send(str.encode('ping'))
 
-# read recieved data
-certificate = s.recv(1024)
+# read and print recieved data
+certificate = s.recv(1024).decode('utf-8')
+print('Certificate recieved', certificate)
 
 # try to find certificate in certificates
 try:
-	value = certificates[certificate.decode('utf-8')]
+	value = certificates[certificate]
 	print(certificate.decode('utf-8'), value)
 	s.send(str.encode('Acknowledged'))
 # if value not found notify user
